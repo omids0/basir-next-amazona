c8db5e79f005b3ce2c0f584638519cae726829f1
omids0 <soheilniao@gmail.com>
2022-03-13 20:50:47 +0330

    first-commit by adding mongodb

---
+++ pages/product/[slug].js
@@ -0,0 +1,120 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import NextLink from "next/link";
+import {
+  Button,
+  Card,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Typography,
+} from "@material-ui/core";
+import useStyles from "../../utils/styles";
+import Image from "next/image";
+import db from "../../utils/db";
+import Product from "../../models/product";
+
+export default function productScreen({product}) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const classes = useStyles();
+
+  if (!product) {
+    return (
+      <div>
+        <h1>Product not found</h1>
+      </div>
+    );
+  }
+
+  return (
+    <Layout title={product.name} description={product.description}>
+      <div className={classes.section}>
+        <NextLink href="/" passHref>
+          <Link>
+            <Typography>Back to products</Typography>
+          </Link>
+        </NextLink>
+      </div>
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography component='h1' variant='h1'>{product.name}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Category: {product.category}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Brand: {product.brand}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>
+                Rating: {product.rating} stars ({product.numReviews} Reviews)
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>description: {product.description}</Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Price</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography> $ {product.price}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Status</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>
+                      {product.countInStock > 0 ? "In stock" : "Unavailable"}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button fullWidth variant="contained" color="primary">
+                  Add to cart
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
+}
